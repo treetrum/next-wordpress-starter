d6d7394499c30be003221504e86230c575bed4db
Sam Davis <sam@sjd.co>
2024-02-16 11:50:10 +1100

    Setup admin url redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
+import "dotenv/config";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  redirects: () => {
+    const adminUrl = process.env.WORDPRESS_URL + "/wp-admin";
+    return [
+      { source: "/admin", destination: adminUrl, permanent: false },
+      { source: "/wp-admin", destination: adminUrl, permanent: false },
+    ];
+  },
+};
 
 export default nextConfig;
