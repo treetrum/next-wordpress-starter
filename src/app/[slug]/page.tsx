07a0bf8f0630f0aae7ebbc2b0d36a3b5b69ae92a
Sam Davis <sam@sjd.co>
2024-02-16 12:00:40 +1100

    Use metadata title templates

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,5 +1,5 @@
 import { PageLayout } from "@/layouts/page";
-import { getPage, getSiteSettings } from "@/server/actions";
+import { getPage } from "@/server/actions";
 
 interface Params {
   slug: string;
@@ -7,11 +7,12 @@ interface Params {
 
 export const generateMetadata = async ({ params }: { params: Params }) => {
   const page = await getPage(params.slug);
-  const meta = await getSiteSettings();
-  return { title: `${page.title ?? "Unknown page"} | ${meta.title}` };
+
+  return { title: page.title };
 };
 
-export default async function Home({ params }: { params: Params }) {
+export default async function Page({ params }: { params: Params }) {
   const page = await getPage(params.slug);
+
   return <PageLayout page={page} />;
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,8 @@
 import { PageLayout } from "@/layouts/page";
-import { getPage, getSiteSettings } from "@/server/actions";
+import { getPage } from "@/server/actions";
 
-interface Params {
-  slug: string;
-}
+export default async function Home() {
+  const page = await getPage("/");
 
-export const generateMetadata = async () => {
-  const meta = await getSiteSettings();
-  return { title: meta.title };
-};
-
-export default async function Home({ params }: { params: Params }) {
-  const page = await getPage(params.slug);
   return <PageLayout page={page} />;
 }
